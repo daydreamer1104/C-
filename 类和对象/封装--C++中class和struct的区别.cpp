55fe1f7a82f178b2988223161b960db87b95467c
daydreamer1104 <1940774852@qq.com>
2025-05-04 13:50:11 +0800

    Add files via upload

---
+++ 类和对象/封装--C++中class和struct的区别.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+//struct默认权限是public，class默认权限是private
+class C1
+{
+	int m_A; // 默认权限 私有private
+};
+struct C2
+{
+	int m_A; //默认权限 公共public
+};
+int main()
+{
+	C1 c1;
+	C2 c2;
+	//c1.m_A = 100;  不能访问
+	c2.m_A = 100;
+	return 0;
+}  
